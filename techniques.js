060aad197bce5a77bdd4c722d5c66a27a0744485
H3K27ac <101502649+H3K27ac@users.noreply.github.com>
2026-01-23 11:21:12 +0800

    Fixed u-substitution

--- techniques.js
+++ techniques.js
@@ -42,7 +42,7 @@ addTechnique("u-substitution", function(){
 
     return {
         integrand: Algebrite.simplify(integrand),
-        solution: Algebrite.integral(g)
+        solution: Algebrite.substitute(f,"x",Algebrite.integral(g))
     };
 });
 
