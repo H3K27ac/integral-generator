67bd897301518ecedad6e9b2f2a370ffb3b2e664
H3K27ac <101502649+H3K27ac@users.noreply.github.com>
2026-01-25 15:10:56 +0800

    Fix

--- techniques.js
+++ techniques.js
@@ -226,7 +226,7 @@ function integrateAxnTrig(a, n, b, c, trig = "sin") {
         const sign = (currentTrig === "sin") ? -1 : 1;
 
         terms.push(
-            forcePositiveCos(`${sign * coeff}/${bPow}*x^${power}*${integratedTrig}`)
+            `${sign * coeff}/${bPow}*x^${power}*${integratedTrig}`
         );
 
         // prepare next IBP round
@@ -245,17 +245,11 @@ function integrateAxnTrig(a, n, b, c, trig = "sin") {
     const finalSign = (currentTrig === "sin") ? -1 : 1;
 
     terms.push(
-        forcePositiveCos(`${finalSign * coeff}/${bPow}*${finalIntegratedTrig}`)
+        `${finalSign * coeff}/${bPow}*${finalIntegratedTrig}`
     );
 
     return terms.join("+").replace(/\+\-/g, "-");
 }
 
-function forcePositiveCos(term) {
-    if (term.includes("cos(") && term.startsWith("-")) {
-        return term.slice(1); // remove leading "-"
-    }
-    return term;
-}
 
 
